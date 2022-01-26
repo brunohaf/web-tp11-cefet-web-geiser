e8a3bd30606f493c4f94134fba961cfe8bc1eb51
Bruno Fonseca <bruno3145@gmail.com>
2022-01-25 22:44:41 -0300

    adds: dynamic viewing for player pages  :sparkles:

--- server/app.js
+++ server/app.js
@@ -32,24 +32,67 @@ let core_path = process.cwd()
 //app.set('views', './views')
 app.set('view engine', 'hbs');
 app.set('views', core_path + '\\server\\views');
-let players = JSON.parse(fs.readFileSync(core_path + '\\server\\data\\jogadores.json', 'utf8'))
+db.data = JSON.parse(fs.readFileSync(core_path + '\\server\\data\\jogadores.json', 'utf8'))
 app.get('/', function (req, res) {
-    res.render('index', {layout : 'index'})
-  })
+    res.render('index', db.players, (err, html) => {
+        if (err)
+            res.status(500).send('' + err)
+        res.send(html);
+    });
+});
+
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter ~15 linhas de código
+db.data.gamesPerPlayer = JSON.parse(fs.readFileSync(core_path + '\\server\\data\\jogosPorJogador.json', 'utf8'))
+app.get('/jogador/:steamid', (req, res) => {
+    const steamid = req.params.steamid
+    playerData = getPlayerDataBySteamId(steamid)
+    if(!playerData){
+        res.status(404).send(`Player not found`)
+    }
+    res.render('jogador', playerData, (err, html) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        res.send(html);
+    });
+});
 
+const getPlayerDataBySteamId = (steamId) => {
+    try{
+        const player = db.data.players.find(player => player.steamid === steamId);
+        if (!player) {
+            return null;
+        }
+        const gamesPerPlayer = db.data.gamesPerPlayer[steamId];
+        let orderedGameList = gamesPerPlayer.games.sort((a,b) => (a.playtime_forever < b.playtime_forever) ? 1 : ((b.playtime_forever < a.playtime_forever) ? -1 : 0))
+        orderedGameList.map(game => {
+            let fixedGame = game
+            fixedGame.playtime_forever = Math.round(game.playtime_forever/60)
+            return fixedGame;
+        })
+        return {
+            player: player,
+            game_count: gamesPerPlayer.game_count,
+            unplayed: gamesPerPlayer.games.filter(game => game.playtime_forever === 0).length,
+            topFive: orderedGameList.slice(0, 5),
+            favorite: orderedGameList[0]
+        }
+    }
+    catch(e){
+        return null
+    }
+}
 
 // EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
-
 app.use(express.static('client'));
-app.listen(PORT)
 
 // abrir servidor na porta 3000 (constante PORT)
 // dica: 1-3 linhas de código
+app.listen(PORT)
